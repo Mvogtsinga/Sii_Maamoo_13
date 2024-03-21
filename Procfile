60fa0da7d508a864b4801e9c2fc38b3c9ac5785f
Aubin Koenigssohn <mvogtsinga@gmail.com>
2024-03-21 19:54:01 +0000

    herokuapp connect..

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Sii_Maamoo.wsgi
--- Sii_Maamoo/settings.py
+++ Sii_Maamoo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hjls(a95hhl2!gu(f5kd&bmuhk1z!b4!s67ywpdekwym^_iq9c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
